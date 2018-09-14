e5f5a38db579dad06306144e34791d136cf69c2c
Lee Shei Pin <shei.pin@toccostudios.com>
2018-09-14 14:02:50 +0800

    remove redux reducers

--- src/js/store.js
+++
@@ -1,8 +0,0 @@
-import {applyMiddleware, createStore} from "redux";
-// import reduxPromise  from "redux-promise-middleware";
-import reducer from "./reducers/reducers.js";
-
-
-// const middleware = applyMiddleware(reduxPromise());
-
-export default createStore(reducer);
